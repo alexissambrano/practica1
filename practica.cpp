79c9e3dc9ab78772d0bd6470c492e637d651c24a
Laboratorio FISI <LaboratorioFISI@UNMSM.pe>
2019-09-06 12:13:04 -0500

    first commit

---
+++ practica.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n1, n2, suma;
+
+    printf("\n   Introduzca primer numero: " );
+    scanf("%d", &n1 );
+    printf("\n   Introduzca segundo numero: " );
+    scanf("%d", &n2 );
+
+    suma
+     = n1 + n2;
+
+
+    printf( "\n   La suma es: %d", suma );
+
+
+
+    return 0;
+}
